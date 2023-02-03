2e8ea06042d92f65bda89687aad893ced890e1bb
Samantha Genevay <samanthagenevay@gmail.com>
2023-02-02 22:57:21 -0600

    Completed Lab

---
+++ index.js
@@ -0,0 +1,16 @@
+// INSTRUCTIONS // 
+
+// 1) has a function expression called divide
+// 2) divide divides 2000 by 100
+// 3) has an arrow function called square
+// 4) square arrow function takes one parameter and multiplies it times itself
+// 5) has an arrow function called add
+// 6) add arrow function takes two parameters and adds them together
+
+const divide = function() {
+    return 2000 / 100;
+}
+
+const square = x => (x * x);
+
+const add = (a, b) => (a + b);
